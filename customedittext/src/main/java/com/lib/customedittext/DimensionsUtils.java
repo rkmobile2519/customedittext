7fca7ec02f5ea632a1185c2d3dfc944fc3e418e2
MOB-2 <kailash.synsoft@gmail.com>
2019-03-26 18:49:02 +0530

    Initial commit to library

---
+++ customedittext/src/main/java/com/lib/customedittext/DimensionsUtils.java
@@ -0,0 +1,33 @@
+package com.lib.customedittext;
+
+import android.content.Context;
+import android.support.annotation.DimenRes;
+
+/**
+ * @author Kailash Chouhan
+ * @creationDate 07-Mar-18
+ * Copyright © 2018 SynsoftGlobal. All rights reserved.
+ */
+
+public class DimensionsUtils {
+    private DimensionsUtils() throws InstantiationException {
+        throw new InstantiationException("This utility class is created for instantiation");
+    }
+
+    public static float getDimension(Context context, @DimenRes int resourceId) {
+        return context.getResources().getDimension(resourceId);
+    }
+
+    public static int getDimensionPixelSize(Context context, @DimenRes int resourceId) {
+        return context.getResources().getDimensionPixelSize(resourceId);
+    }
+
+    private static float scale;
+
+    public static int dpToPixel(float dp, Context context) {
+        if (scale == 0) {
+            scale = context.getResources().getDisplayMetrics().density;
+        }
+        return (int) (dp * scale);
+    }
+}
